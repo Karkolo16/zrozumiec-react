ed69311197635eeace0e604795632e59652777a3
Karolina <ka.kolomyjec@gmail.com>
2023-09-30 18:33:42 +0200

    Lesson 8th

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,4 @@
 import ReactDOM from "react-dom/client";
-import { LinkComponent } from "./components/Link";
-import { HeadingItalic } from "./components/HeadingItalic";
-import { Logger } from "./components/Logger";
-const element = (
-  <>
-    <Logger>
-      <HeadingItalic>Nasza Strona</HeadingItalic>
-    </Logger>
-    <main>
-      <LinkComponent text="Sprawdź stronę kursu!" shouldOpenNewTab={true} />
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt
-        deleniti numquam perferendis voluptatum voluptate impedit officiis.
-        Quibusdam laborum cum reprehenderit.
-      </p>
-      <LinkComponent shouldOpenNewTab={false} />
-    </main>
-  </>
-);
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(element);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
